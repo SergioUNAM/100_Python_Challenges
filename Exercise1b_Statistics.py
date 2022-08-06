40c01623d21dd8565a70955534a5b5bec3601112
SergioUNAM <hhser@comunidad.unam.mx>
2022-08-05 20:13:44 -0500

    Exercise1b_Statistics.py

---
+++ Exercise1b_Statistics.py
@@ -0,0 +1,7 @@
+"""Count as well as sum up the natural numbers that are divisible by 2 or 7 up to a given maximun value (exclusive) and output it to the console. Write a function calc_sum_and_count_all_numbers_div_by_2_or_7(max_exclusive). Extend it so that it returns the two values instead of performing the console output.
+
+For example Maximun: 8; Divisible by 2: 2, 4, 6; Divisible by 7: 7; Count: 4; Sum: 2 + 4 + 5 + 7 = 19
+
+"""
+
+""""Contar así como sumar los números naturales que son divisibles por 2 o 7 hasta un valor máximo dado (exclusivo) y enviarlo a la consola. Escribe una función calc_sum_and_count_all_numbers_div_by_2_or_7(max_exclusive). Extiéndela para que devuelva los dos valores en lugar de realizar la salida por consola"""
