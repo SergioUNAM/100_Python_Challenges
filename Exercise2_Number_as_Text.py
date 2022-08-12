f745a8f9906200b3c3c052091c70ede4c0aee40b
SergioUNAM <hhser@comunidad.unam.mx>
2022-08-11 22:42:45 -0500

    Exercise2_Number_as_Text.py

--- Exercise2_Number_as_Text.py
+++ Exercise2_Number_as_Text.py
@@ -7,10 +7,22 @@ def number_as_text(n):
     while remaining_value > 0:
         remainder_as_text = digit_as_text(remaining_value % 10)
         remaining_value = int(remaining_value / 10)
-        value = remainder_as_text + "" + value
+        value = remainder_as_text + " " + value
 
     return value.strip()
 
-value_to_text_mapping = {
-    0: "Zero", 1: "ONE", 2: "TWO", 3: "THREE", 4
-}
+
+value_to_text_mapping = {0: "Zero", 1: "ONE", 2: "TWO", 3: "THREE", 4: "FOUR", 5: "FIVE", 6: "SIX", 7: "SEVEN",
+                         8: "EIGHT", 9: "NINE"}
+
+
+def digit_as_text(n):
+    return value_to_text_mapping[n % 10]
+
+
+def run():
+    print(number_as_text(48))
+
+
+if __name__ == "__main__":
+    run()
